f50335eb38f37862de4e8c6f82d84a259b0d29b5
bezat <61169260+bezata@users.noreply.github.com>
2022-08-16 16:13:48 +0300

    Update chipLottery.sol

--- contracts/chipLottery.sol
+++ contracts/chipLottery.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract chipLottery is ERC721,Ownable{
     address public owners;
     uint256 chipsPrice = 0.001 ether;
@@ -17,7 +18,7 @@ contract chipLottery is ERC721,Ownable{
     mapping (uint => address payable) public betHistory;
 
     constructor() payable ERC721('Roulette Game','RG'){
-      maxChipChecker = 4;
+      maxChipChecker = 0;
       betID = 1;
       owners = msg.sender;
     }
@@ -36,8 +37,9 @@ contract chipLottery is ERC721,Ownable{
     }
     // Minting roulette chips
     function buyChips() public payable{
-        require(msg.value < chipsPrice,'Insufficient ballance');
+        require(msg.value != chipsPrice,'Insufficient balance');
         require(maxChipChecker > rouletteChips,'All chips sold');
+        require(walletChecker[msg.sender] < 1 ,'You can only buy one chip');
 
         uint256 tokenId = rouletteChips;
         _safeMint(msg.sender,tokenId);
@@ -59,13 +61,14 @@ contract chipLottery is ERC721,Ownable{
     // joining the lottery
     function enterthelottery() public payable {
         require(walletChecker[msg.sender] > 0,'Insufficient balance sry!');
+        walletChecker[msg.sender]--;
         require(startRound, 'Round not started');
     // players adress entering the lottery
         players.push(payable(msg.sender));
     }
     //Random number function
     function getRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(owners,block.timestamp)));
+        return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty,players.length,betHistory[betID])));
     }
     // Picking winner
     function checkWinner() public onlyOwner {
@@ -76,5 +79,3 @@ contract chipLottery is ERC721,Ownable{
     }
 
 }
- 
- 

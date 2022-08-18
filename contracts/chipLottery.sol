38c678ab1dc4df3f122d7e6e13c8686347a8cd54
bezat <61169260+bezata@users.noreply.github.com>
2022-08-18 16:49:18 +0300

    Update chipLottery.sol

--- contracts/chipLottery.sol
+++ contracts/chipLottery.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-
-contract chipLottery is ERC721,Ownable{
+contract chipLottery is Ownable,VRFConsumerBaseV2,ERC721{
+    VRFCoordinatorV2Interface public immutable COORDINATOR;
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 4;
+    uint32 numWords =  1;
+    uint64 s_subscriptionId;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    uint256 public winningNumber;
+    address s_owner;
     address public owners;
     uint256 chipsPrice = 0.001 ether;
     uint256 rouletteChips;
@@ -17,11 +29,27 @@ contract chipLottery is ERC721,Ownable{
     uint public betID;
     mapping (uint => address payable) public betHistory;
 
-    constructor() payable ERC721('Roulette Game','RG'){
-      maxChipChecker = 0;
+    constructor(uint64 subscriptionId) payable VRFConsumerBaseV2(vrfCoordinator) ERC721('Roulette Game','RG'){
+      maxChipChecker = 4;
       betID = 1;
       owners = msg.sender;
+      COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+      s_owner = msg.sender;
+      s_subscriptionId = subscriptionId;
     }
+    //VRF
+    function requestRandomWords() external onlyOwner {
+      s_requestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      s_subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+  }
+    function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override{
+      winningNumber = (randomWords[0] % 4) + 1;
+  }
     //Starting the round
     function RoundStarter() public onlyOwner{
         startRound = true;
@@ -37,8 +65,9 @@ contract chipLottery is ERC721,Ownable{
     }
     // Minting roulette chips
     function buyChips() public payable{
-        require(msg.value != chipsPrice,'Insufficient balance');
+        require(msg.value < chipsPrice,'Insufficient ballance');
         require(maxChipChecker > rouletteChips,'All chips sold');
+
         uint256 tokenId = rouletteChips;
         _safeMint(msg.sender,tokenId);
         walletChecker[msg.sender]++;
@@ -63,16 +92,17 @@ contract chipLottery is ERC721,Ownable{
     // players adress entering the lottery
         players.push(payable(msg.sender));
     }
-    //Random number function
-    function getRandomNumber() public view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty,players.length,betHistory[betID])));
-    }
+    //Old random number function
+    /*function getRandomNumber() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(owners,block.timestamp)));
+    }*/
     // Picking winner
     function checkWinner() public onlyOwner {
-        uint index = getRandomNumber() % players.length;
+        uint index = winningNumber;
         walletChecker[players[index]]++;
         betHistory[betID] = players[index];
         betID++;
     }
-
 }
+ 
+ 
